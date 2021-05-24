c05e9a53a964dedfeb11f35d15e0d3efa1a70ff6
khanmaster <eashkhan@hotmail.com>
2021-05-24 11:39:44 +0100

    built in packages with import added

---
+++ python_modules.py
@@ -0,0 +1,19 @@
+# What are python Modules
+# We have  built in Modules that we can find on Python's Library
+# if the value is 1.49 floor() 1 ,  ceil()1.51 2
+# we have a keyword called import that we can use to call a module
+
+from random import random
+import math
+
+print(random())
+num1 = 23.66
+print(num1)
+
+# if the user input is >= .50 apply ceil() - use random() to generate a number
+# if the user input is <= .49 apply floor()
+
+print(math.ceil(num1))
+print(math.floor(num1))
+# what is the use case
+#
