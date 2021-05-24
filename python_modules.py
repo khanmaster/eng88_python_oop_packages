4ec506ad91465dcab078608c6d0e4b1f9b211d72
khanmaster <eashkhan@hotmail.com>
2021-05-24 12:13:17 +0100

    os sys modules packages added

--- python_modules.py
+++ python_modules.py
@@ -1,19 +1,62 @@
-# What are python Modules
-# We have  built in Modules that we can find on Python's Library
-# if the value is 1.49 floor() 1 ,  ceil()1.51 2
-# we have a keyword called import that we can use to call a module
+# # What are python Modules
+# # We have  built in Modules that we can find on Python's Library
+# # if the value is 1.49 floor() 1 ,  ceil()1.51 2
+# # we have a keyword called import that we can use to call a module
+#
+# from random import random
+# import math
+#
+# print(random())
+# num1 = 23.66
+# print(num1)
+#
+# # if the user input is >= .50 apply ceil() - use random() to generate a number
+# # if the user input is <= .49 apply floor()
+#
+# print(math.ceil(num1))
+# print(math.floor(num1))
+# # what is the use case
+# os , sys are used to get information about your localhost/your machine such as name, path etc
 
-from random import random
-import math
+import os, sys
 
-print(random())
-num1 = 23.66
-print(num1)
+# working_dir = os.getcwd()
+# print("This is your current working Dir " + working_dir)
+#
+# system_path = sys.path
+# print("This is the path " + str(system_path))
 
-# if the user input is >= .50 apply ceil() - use random() to generate a number
-# if the user input is <= .49 apply floor()
+def current_system_path():
+    print("This is your current path ")
+    return sys.path
+print(current_system_path())
 
-print(math.ceil(num1))
-print(math.floor(num1))
-# what is the use case
 
+
+
+
+
+
+
+
+
+
+
+
+
+# from random import random
+# import math
+#
+# num1 = random()
+# print(num1)
+# if num1 >= 0.5:
+#     print(math.ceil(num1))
+# else:
+#     print(math.floor(num1))
+
+# import os, sys
+# def current_system_path():
+#     print(" this is your current directory ")
+#     return sys.path
+#
+# print(current_system_path())
